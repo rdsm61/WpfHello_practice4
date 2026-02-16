714e458ca9d262b6746d6815d4308e8f6fbf4fb7
rdsm61 <rdsameoff@gmail.com>
2026-02-17 00:11:39 +0300

    Добавьте файлы проекта.

---
+++ WpfHello/CustomCommands.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace WpfHello
+{
+    class CustomCommands
+    {
+        public static RoutedUICommand Launch { get; }
+
+        static CustomCommands()
+        {
+            InputGestureCollection myInputGesture = new InputGestureCollection();
+            myInputGesture.Add(new KeyGesture(Key.L, ModifierKeys.Control));
+            Launch = new RoutedUICommand("Запуск", "Launch", typeof(CustomCommands), myInputGesture);
+        }
+    }
+}
---
+++ WpfHello/MainWindow.xaml.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfHello
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        bool isDataDirty = false;
+        string nameFile = "username.txt";
+
+        public MyWindow myWin { get; set; }
+       
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            CommandBinding abinding = new CommandBinding();
+            abinding.Command = CustomCommands.Launch;
+            abinding.Executed += new ExecutedRoutedEventHandler(Launch_Handler);
+            abinding.CanExecute += new CanExecuteRoutedEventHandler(LaunchEnabled_Handler);
+            this.CommandBindings.Add(abinding);
+
+            lbl.Content = "Добрый день!";
+            setBut.IsEnabled = false;
+            retBut.IsEnabled = false;
+            Top = 25;
+            Left = 25;
+        }
+
+        private void LaunchEnabled_Handler(object sender, CanExecuteRoutedEventArgs e)
+        {
+            e.CanExecute = (bool)check.IsChecked;
+        }
+
+        private void Launch_Handler(object sender, ExecutedRoutedEventArgs e)
+        {
+            if (myWin == null)
+            {
+                myWin = new MyWindow();
+            }
+            myWin.Owner = this;
+            //    myWin.Top = this.Top;
+            //    myWin.Left = this.Left + this.Width;
+
+            var location = New_Win.PointToScreen(new Point(0, 0));
+            myWin.Left = location.X + myWin.Width / 2;
+            myWin.Top = location.Y + 20;
+            myWin.Show();
+        }
+
+        private void SetBut()
+        {
+            System.IO.StreamWriter sw = null;
+            sw = new System.IO.StreamWriter(nameFile);
+            sw.WriteLine(setText.Text);
+            sw.Close();
+            retBut.IsEnabled = true;
+            isDataDirty = false;
+            
+        }
+
+        private void RetBut()
+        {
+            System.IO.StreamReader sr = null;
+            sr = new System.IO.StreamReader(nameFile);
+            retLabel.Content = "Приветствую Вас, уважаемый " + sr.ReadToEnd();
+            sr.Close();
+        }
+
+        private void setText_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            setBut.IsEnabled = true;
+            isDataDirty = true;
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            if (isDataDirty)
+            {
+                string msg = "Данные были изменены, но не сохранены. \nЗакрыть окно без сохранения?";
+                MessageBoxResult result = MessageBox.Show(msg, "Контроль данных", MessageBoxButton.YesNo, MessageBoxImage.Warning);
+
+                if (result == MessageBoxResult.No)
+                {
+                    e.Cancel = true;
+                }
+            }
+        }
+
+        private void MenuItem_Click_1(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void Window_Closed(object sender, EventArgs e)
+        {
+            this.myWin = null;
+        }
+
+        private void Grid_Click(object sender, RoutedEventArgs e)
+        {
+            FrameworkElement feSource = e.Source as FrameworkElement;
+
+            switch (feSource.Name)
+            {
+                case "setBut":
+                    SetBut();
+                    break;
+                case "retBut":
+                    RetBut();
+                    break;
+            }
+
+            e.Handled = true;
+        }
+    }
+}
